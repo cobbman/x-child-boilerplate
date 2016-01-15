3f5a0e395b47332ed97ae6004be83271fea163f6
William <hello@bigwilliam.com>
2016-01-14 20:20:22 -0700

    gulp - add cssnano and livereload. make 'build' task

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,65 @@
-// Include gulp
-var gulp = require('gulp');
+var gulp         = require('gulp');
+var gutil        = require('gulp-util');
+var notify       = require('gulp-notify');
+var less         = require('gulp-less');
+var autoprefixer = require('gulp-autoprefixer');
+var cssnano      = require('gulp-cssnano');
+var sourcemaps   = require('gulp-sourcemaps');
+var livereload   = require('gulp-livereload');
 
-// Include packages
-var less = require('gulp-less');
-var path = require('path');
+//var exec = require('child_process').exec;
+//var sys = require('sys');
 
-// Less Task
-gulp.task('less', function() {
-  return gulp.src('./assets/less/*.less')
-    .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
-    }))
-    .pipe(gulp.dest('./assets/css'));
+// Where do you store your Sass files?
+var lessDir = 'assets/less';
+
+// Which directory should LESS compile to?
+var targetCSSDir = 'assets/css';
+
+
+// Which directory should CoffeeScript compile to?
+var targetJSDir = 'assets/js';
+
+
+// Compile LESS for dev
+gulp.task('css', function () {
+  return gulp.src(lessDir + '/main.less')
+		.pipe( less() ).on( 'error', gutil.log )
+    .pipe( gulp.dest(targetCSSDir) )
+    .pipe( notify('LESS Files built and Page Reloaded') )
+    .pipe( livereload() );
 });
 
-// Watch files for changes
-gulp.task('watch', function() {
-  gulp.watch('./assets/less/*.less', ['less']);
+// Compile LESS and Minify CSS for production
+gulp.task('build', function () {
+  return gulp.src(lessDir + '/main.less')
+		.pipe( sourcemaps.init() )
+		.pipe( less() ).on( 'error', gutil.log )
+    .pipe( cssnano() ).on( 'error', gutil.log )
+  //   .pipe(autoprefixer({
+		// 	browsers: ['last 2 versions'],
+		// 	cascade: true
+		// }))
+		.pipe( sourcemaps.write('.') )
+    .pipe( gulp.dest(targetCSSDir) )
+    .pipe( notify('Less files compiled and MINIFIED for Production') )
+    .pipe( livereload() );
 });
 
-// Default Task
-// gulp.task('default', ['less']);
+
+// Run all PHPUnit tests
+//gulp.task('phpunit', function() {
+//    exec('phpunit', function(error, stdout) {
+//        sys.puts(stdout);
+//    });
+//});
+
+// Keep an eye on Sass, Coffee, and PHP files for changes...
+gulp.task('watch', function () {
+	livereload.listen();
+  gulp.watch(lessDir + '/*.less', ['css']);
+  //gulp.watch('app/**/*.php', ['phpunit']);
+});
+
+// What tasks does running gulp trigger?
+gulp.task('default', ['css']);
